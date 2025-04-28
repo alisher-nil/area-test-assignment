6e50f8636c5fe4c464429d300f027aadf571c11d
Alisher Nildibayev <alisher.nil@gmail.com>
2025-04-29 03:07:36 +0500

    Add triangle area calculation function using Heron's formula

--- src/area_test_assignment/geometry.py
+++ src/area_test_assignment/geometry.py
@@ -1,5 +1,7 @@
-from math import pi
+from math import pi, sqrt
 from typing import Union
+
+Number = Union[int, float]
 
 
 def circle_area(radius: Union[int, float]) -> float:
@@ -7,3 +9,21 @@ def circle_area(radius: Union[int, float]) -> float:
     if radius < 0:
         raise ValueError("Radius cannot be negative")
     return pi * radius**2
+
+
+def triangle_area(side_a: Number, side_b: Number, side_c: Number) -> float:
+    """
+    Calculate the area of a triangle given its three sides
+    using Heron's formula.
+    """
+    if side_a <= 0 or side_b <= 0 or side_c <= 0:
+        raise ValueError("Sides must be positive")
+    semiperimeter = (side_a + side_b + side_c) / 2
+    return sqrt(
+        (
+            semiperimeter
+            * (semiperimeter - side_a)
+            * (semiperimeter - side_b)
+            * (semiperimeter - side_c)
+        )
+    )
